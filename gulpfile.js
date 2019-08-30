02c01fe0a9f4caf78305a18e8e1e94dec620e19a
Anastasia Videneeva <nastya.videneeva@gmail.com>
2019-08-30 13:05:18 -0700

    Adds automatization

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,17 @@
 "use strict";
 
 var gulp = require("gulp");
+var del = require("del");
 var plumber = require("gulp-plumber");
 var sourcemap = require("gulp-sourcemaps");
+var rename = require("gulp-rename");
+var csso = require("gulp-csso");
 var sass = require("gulp-sass");
+var terser = require("gulp-terser");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var imagemin = require("gulp-imagemin");
+var webp = require("gulp-webp");
 var server = require("browser-sync").create();
 
 gulp.task("css", function () {
@@ -16,14 +22,30 @@ gulp.task("css", function () {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
     .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
 });
 
+gulp.task("js", function () {
+  return gulp.src("source/js/menu.js")
+    .pipe(sourcemap.init())
+    .pipe(gulp.dest("source/js"))
+    .pipe(gulp.dest("build/js"))
+    .pipe(terser())
+    .pipe(rename("script.min.js"))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("source/js"))
+    .pipe(gulp.dest("build/js"))
+    .pipe(server.stream());
+})
+
 gulp.task("server", function () {
   server.init({
-    server: "source/",
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
@@ -31,7 +53,64 @@ gulp.task("server", function () {
   });
 
   gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
-  gulp.watch("source/*.html").on("change", server.reload);
+  gulp.watch("source/js/*.js", gulp.series("js"));
+  gulp.watch(("source/*.html"), gulp.series("html", "reload"));
 });
 
-gulp.task("start", gulp.series("css", "server"));
+gulp.task("images", function () {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({
+        optimizationLevel: 3
+      }),
+      imagemin.jpegtran({
+        progressive: true
+      }),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("webp", function () {
+  return gulp.src("source/img/**/*.{png,jpg}")
+    .pipe(webp({
+      quality: 90
+    }))
+    .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("html", function () {
+  return gulp.src("source/*.html")
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("copy", function () {
+  return gulp.src([
+      "source/fonts/**/*.{woff,woff2}",
+      "source/img/**",
+      "source/js/*.js",
+      "source/css/*.css"
+    ], {
+      base: "source"
+    })
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function () {
+  return del("build");
+});
+
+gulp.task("reload", function (done) {
+  server.reload();
+  done();
+});
+
+gulp.task("build", gulp.series(
+  "clean",
+  "copy",
+  "css",
+  "js",
+  "html"
+));
+
+gulp.task("start", gulp.series("clean", "copy", "css", "js", "html", "server"));
