9013b655191714c8896fed034a9aab41638ca3b4
Anastasia Videneeva <nastya.videneeva@gmail.com>
2019-08-31 17:18:26 -0700

    Index page: adds link to business-button

--- source/js/business.js
+++ source/js/business.js
@@ -4,6 +4,7 @@
 const businessButton = document.querySelector('.plan__button-business');
 const businessModal = document.querySelector('.plan-business');
 
-businessButton.addEventListener('click', function () {
+businessButton.addEventListener('click', function (evt) {
+  evt.preventDefault();
   businessModal.classList.toggle('plan-business--closed')
 });
