ae629a4741fd821f3977a183d26e71baa3055adf
Anastasia Videneeva <nastya.videneeva@gmail.com>
2019-09-01 11:47:55 -0700

    Fixes business modal code

--- source/js/business.js
+++ source/js/business.js
@@ -3,8 +3,14 @@
 /* Business plans modal */
 const businessButton = document.querySelector('.plan__button-business');
 const businessModal = document.querySelector('.plan-business');
+const businessModalClose = businessModal.querySelector('.plan__button-close');
 
 businessButton.addEventListener('click', function (evt) {
   evt.preventDefault();
-  businessModal.classList.toggle('plan-business--closed')
+  businessModal.classList.toggle('plan-business--closed');
 });
+
+businessModalClose.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  businessModal.classList.toggle('plan-business--closed');
+})
