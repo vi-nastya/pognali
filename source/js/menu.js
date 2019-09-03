52c39d2478ce9b9b3484cf9a8ba375fd0ce520b7
Anastasia Videneeva <nastya.videneeva@gmail.com>
2019-09-03 08:55:27 -0700

    Header: fixes menu states

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,21 +1,11 @@
 'use strict';
 
-const logoBlue = document.querySelector('.header__logo-image--blue');
-const logoWhite = document.querySelector('.header__logo-image--white');
-
-const changeLogo = () => {
-  logoBlue.classList.toggle('visually-hidden');
-  logoWhite.classList.toggle('visually-hidden');
-}
-
 const header = document.querySelector('.header');
 header.classList.remove('header--nojs');
-header.classList.remove('header--open-menu');
 
 const menuButton = document.querySelector('.header__nav-toggle');
 
 menuButton.addEventListener('click', function () {
-  header.classList.toggle('header--white');
   header.classList.toggle('header--open-menu');
   changeLogo();
 });
